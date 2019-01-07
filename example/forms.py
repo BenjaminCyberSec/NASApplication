301db895b0aca642764ca76557c1892e4880e339
benjamin Berge <benjamin.berge@ulb.be>
2019-01-07 15:22:08 +0100

    sharedfile form fixed

--- example/forms.py
+++ example/forms.py
@@ -5,24 +5,24 @@ from .models import File
 
 
 class FileForm(forms.Form):
-     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+     file_field = forms.FileField(label='Select the file(s) you want to add',widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
 class KeyForm(forms.Form):
      password = forms.CharField(widget=forms.PasswordInput())
 
 class OwnerForm(forms.Form):
      name = forms.CharField(
-        label='Owner Name',
+        label='Add a user by his username',
         widget=forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': 'Enter the correct owner\' name here'
+            'placeholder': 'Enter the correct owner\'s name here'
         })
      )
 OwnerFormSet = formset_factory(OwnerForm, extra=1)
 
 class SharedFileForm(forms.Form):
-     minimum_validation = forms.IntegerField()
-     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+     minimum_validation = forms.IntegerField(label='Enter the minimum users required to unlock the file (at least 2)')
+     file_field = forms.FileField(label='Select the file(s) you want to add',widget=forms.ClearableFileInput(attrs={'multiple': True}))
      #OwnerFormSet = formset_factory(OwnerForm, extra=2)
      
 
